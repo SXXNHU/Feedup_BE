2662647106e70bd24a88993f4ec63f17f9049b8f
SXXHU <dofqls0127@gmail.com>
2025-08-13 04:30:37 +0900

    chore: 회원가입 관련 클래스명 변경

--- feedup/src/main/java/ll25/feedup/Host/service/HostSignupService.java
+++ feedup/src/main/java/ll25/feedup/Host/service/HostSignupService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class HostService {
+public class HostSignupService {
 
     private final HostRepository hostRepository;
 
